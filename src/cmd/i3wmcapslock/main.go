74b92084cde0da9a9e8a7c41729f8d2153ad0439
Carmine Di Monaco <carmine.dimonaco@gmail.com>
2019-10-18 16:19:51 +0200

    Led caps lock status retrieving and setting in i3bar

--- src/cmd/i3wmcapslock/main.go
+++ src/cmd/i3wmcapslock/main.go
@@ -5,18 +5,38 @@ import (
 	"os"
 
 	"github.com/cdimonaco/i3wmcapslock/src/i3bar"
+	"github.com/cdimonaco/i3wmcapslock/src/led"
 )
 
 func main() {
-	i3Client := i3bar.NewClient(
+	i3Client, err := i3bar.NewClient(
 		os.Stdin,
 		os.Stdout,
 	)
+	if err != nil {
+		fmt.Printf("Fatal error %s", err.Error())
+	}
 	blockChan := i3Client.Start()
 	for blocks := range blockChan {
-		//Blocks represents the last protocol block
-		lastBlock := blocks[len(blocks)-1]
-		i3Client.AppendBlock(blocks, lastBlock)
-		fmt.Print(",")
+		mask, err := led.Status()
+		// Check caps status
+		capsLock := led.CapsLock()
+		capsBlock := i3bar.Block{
+			FullText: "Caps",
+			Name:     "capslock",
+			Color:    capsLock.Color().Inactive,
+		}
+		if mask&capsLock.Mask() != 0 {
+			capsBlock = i3bar.Block{
+				FullText: "Caps",
+				Name:     "capslock",
+				Color:    capsLock.Color().Active,
+				Urgent:   true,
+			}
+		}
+		if err != nil {
+			panic(err.Error())
+		}
+		i3Client.AppendBlock(blocks, &capsBlock, true)
 	}
 }
